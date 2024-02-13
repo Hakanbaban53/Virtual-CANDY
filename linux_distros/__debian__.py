61966e26bd9bbed71cc9b5505f764f221d0a78e5
Hakan İSMAİL <hakanismail53@gmail.com>
2024-02-13 20:44:41 +0300

    Update __debian__.py

--- linux_distros/__debian__.py
+++ linux_distros/__debian__.py
@@ -44,6 +44,17 @@ def type_of_action(data):
                     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                     sudo apt-get update
                 """
+                script2 = """
+                    sudo apt install curl wget gnupg2 lsb-release -y
+                    curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/vbox.gpg
+                    curl -fsSL https://www.virtualbox.org/download/oracle_vbox.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox.gpg
+
+                    echo \
+                    "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+                    sudo apt update
+                    sudo apt install linux-headers-$(uname -r) dkms -y
+                    sudo apt install virtualbox-7.0 -y
+                """
                 os.system(script)
                 print("Docker repository keys installed successfully.")
             except subprocess.CalledProcessError as err:
