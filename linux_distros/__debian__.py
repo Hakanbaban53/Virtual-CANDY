a10d11ea0b9e892e96caf37d07b5dbb94771db81
Hakan İSMAİL <hakanismail53@gmail.com>
2024-02-13 21:12:53 +0300

    Debian and ubuntu Finished!

--- linux_distros/__debian__.py
+++ linux_distros/__debian__.py
@@ -1,5 +1,4 @@
 from os import getenv
-import os
 import subprocess
 
 
--- linux_distros/__ubuntu__.py
+++ linux_distros/__ubuntu__.py
@@ -1,5 +1,4 @@
 from os import getenv
-import os
 import subprocess
 
 
@@ -23,26 +22,19 @@ def type_of_action(data):
     target_directory = f"/home/{current_user}/"
     type = data.get("type", "")
     value = data.get("value", "")
-    name = data.get("name", "")
     try:
         if type == "install-package":
             packages_to_install = value.split()  # Split the package names into a list
             subprocess.call(["sudo", "apt", "install"] + packages_to_install)
 
         elif type == "get-keys":
-            try:
-                script = """
-                sudo apt-get install -y ca-certificates curl &&
-                sudo install -m 0755 -d /etc/apt/keyrings &&
-                sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
-                sudo chmod a+r /etc/apt/keyrings/docker.asc &&
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                """
-                os.system(script)
-                print("Docker repository keys installed successfully.")
-            except subprocess.CalledProcessError as err:
-                print(f"An error occurred: {err}")
+            keys = data["script"]
+            for command in keys:
+                try:
+                    subprocess.run(command, shell=True, check=True)
+                    print("Script executed successfully.")
+                except subprocess.CalledProcessError as err:
+                    print(f"An error occurred: {err}")
             
 
         elif type == "local-package":
@@ -55,10 +47,16 @@ def type_of_action(data):
                     f"{target_directory}local.package.deb",
                     value,
                 ],
-                check=True
+                check=True,
             )
             subprocess.run(
-                ["sudo", "apt-get", "--fix-broken", "install", f"{target_directory}local.package.deb"],
+                [
+                    "sudo",
+                    "apt-get",
+                    "--fix-broken",
+                    "install",
+                    f"{target_directory}local.package.deb",
+                ],
                 check=True,
             )
 
@@ -67,7 +65,9 @@ def type_of_action(data):
             subprocess.call(["sudo", "apt", "remove"] + packages_to_remove)
 
         elif type == "add-repo-flathub":
-            subprocess.call(["flatpak", "remote-add", "--if-not-exists", "flathub", value])
+            subprocess.call(
+                ["flatpak", "remote-add", "--if-not-exists", "flathub", value]
+            )
 
         elif type == "install-service":
             subprocess.call(["sudo", "systemctl", "restart", value])
