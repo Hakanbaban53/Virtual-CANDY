28439eb5254380170d76f0c1bec4d1eaeb46eeed
Hakan İSMAİL <hakanismail53@gmail.com>
2024-02-13 15:19:57 +0300

    flatpak package added

--- __cli_dependencies_install__.py
+++ __cli_dependencies_install__.py
@@ -2,25 +2,17 @@ import subprocess
 from __pip_install__ import pip_package_installer
 from __get_os_package_manager__ import get_linux_package_manager
 
-dependencies = ["click"]
+dependencies = []
 
 
 def check_dependencies():
     """Install required dependencies for the CLI app if not already installed."""
 
-    dependencies = ["pip"]
-
     for package in dependencies:
         try:
             subprocess.check_call(["pip", "show", package], stdout=subprocess.PIPE)
-        except subprocess.CalledProcessError as e:
-            # If an error occurs
-            if "externally-managed-environment" in str(e):
-                print("Error: externally-managed-environment detected. Removing the file and retrying.")
-                subprocess.run(['sudo', 'rm', '/usr/lib/python3.11/EXTERNALLY-MANAGED'])
-                check_dependencies()
-            else:
-                return False
+        except subprocess.CalledProcessError:
+            return False
     return True 
 
 
@@ -41,7 +33,7 @@ def get_dependencies():
         )
         if selection.lower() == "y" or not selection:
             pip_package_installer(dependencies)
-            get_linux_package_manager()
+            #get_linux_package_manager()
         elif selection.lower() == "n":
             print("Can you want to continue installing[y/N] ?")
         else:
