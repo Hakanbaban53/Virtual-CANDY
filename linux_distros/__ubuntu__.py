3c190753dbb8e3ea0ae9a2219ef931205ea2b211
Hakan İSMAİL <hakanismail53@gmail.com>
2024-02-13 17:36:40 +0300

    Docker repo added and fixing the local package installer.

--- linux_distros/__ubuntu__.py
+++ linux_distros/__ubuntu__.py
@@ -33,7 +33,6 @@ def type_of_action(data):
 
         elif type == "get-keys":
             try:
-                # Multiline shell script
                 script = """
                 sudo apt-get install -y ca-certificates curl &&
                 sudo install -m 0755 -d /etc/apt/keyrings &&
@@ -42,7 +41,7 @@ def type_of_action(data):
                 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
                 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
                 """
-                subprocess.run(script, shell=True, check=True, executable="/bin/bash")
+                os.system(script)
                 print("Docker repository keys installed successfully.")
             except subprocess.CalledProcessError as err:
                 print(f"An error occurred: {err}")
@@ -55,19 +54,22 @@ def type_of_action(data):
                     "--show-progress",
                     "--progress=bar:force",
                     "-O",
-                    f"{name}.package.deb",
+                    f"{target_directory}local.package.deb",
                     value,
                 ],
-                cwd=target_directory,
+                check=True
             )
             subprocess.run(
-                ["sudo", "apt-get", "--fix-broken", "install", f"{name}.package.deb"],
-                cwd=target_directory,
+                ["sudo", "apt-get", "--fix-broken", "install", f"{target_directory}local.package.deb"],
+                check=True,
             )
 
         elif type == "remove-package":
             packages_to_remove = value.split()  # Split the package names into a list
             subprocess.call(["sudo", "apt", "remove"] + packages_to_remove)
+
+        elif type == "add-repo-flathub":
+            subprocess.call(["flatpak", "remote-add", "--if-not-exists", "flathub", value])
 
         elif type == "install-service":
             subprocess.call(["sudo", "systemctl", "restart", value])
