d397fc24d80850b969f9c5d2ca1ff533b03049bf
Hakan İSMAİL <hakanismail53@gmail.com>
2024-03-08 11:22:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from __arguments__ import parse_arguments
 from __check_repository_connection__ import check_linux_package_manager_connection
+from __cli_dependencies_install__ import handle_dependencies
 from __get_os_package_manager__ import get_linux_package_manager
 from __get_os_package_manager__ import identify_distribution
 
@@ -34,5 +35,6 @@ def print_info(args):
 
 if __name__ == "__main__":
     linux_distro_id = identify_distribution()
-    #check_linux_package_manager_connection(linux_distro_id)
+    handle_dependencies()
+    check_linux_package_manager_connection(linux_distro_id)
     app()
