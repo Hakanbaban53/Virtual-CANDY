a1a5de28788bc47e71fcdc3724bb78cfa5d28f2e
Hakan İSMAİL <hakanismail53@gmail.com>
2024-07-01 14:56:31 +0300

    Added the install vmware kernel modules

--- functions/__vmware_install__.py
+++ functions/__vmware_install__.py
@@ -16,13 +16,29 @@ def download_file(url, filename):
     run_command(f"wget -O {filename} {url}")
     print(f"Downloaded {filename}.")
 
-def extract_tar(filename, exrated_dir):
+def extract_tar(filename, extracted_dir):
     """Extract a tar file."""
-    print(f"Extracting {filename} to {exrated_dir}...")
-    run_command(f"tar -xf {filename} -C {exrated_dir}")
+    print(f"Extracting {filename} to {extracted_dir}...")
+    run_command(f"tar -xf {filename} -C {extracted_dir}")
+
+def install_vmware_modules():
+    # Clone the repository for VMware host modules
+    print("Cloning vmware-host-modules repository...")
+    run_command("git clone -b tmp/workstation-17.5.2-k6.9.1 https://github.com/nan0desu/vmware-host-modules.git")
+    
+    # Navigate into the cloned directory
+    os.chdir("vmware-host-modules")
+    
+    # Make tarballs and copy to the VMware modules source directory
+    print("Making and copying vmmon.tar and vmnet.tar...")
+    run_command("sudo make tarballs && sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/")
+    
+    # Run vmware-modconfig to install all modules
+    print("Running vmware-modconfig to install all modules...")
+    run_command("sudo vmware-modconfig --console --install-all")
 
 def install_vmware_fedora():
-    # Set up URLs and filenames
+    # Set up URLs and filenames for VMware Workstation
     pkgver = "17.5.2"
     buildver = "23775571"
     tools_version = "12.4.0-23259341"
@@ -48,10 +64,11 @@ def install_vmware_fedora():
     for url, filename in zip(component_urls, component_filenames):
         download_file(url, filename)
     
-    # # Step 2: Extract the bundle file
+    # Step 2: Extract the bundle file
     print("Extracting the bundle file...")
     run_command(f"tar -xf {bundle_filename}")
 
+    # Extracted components directory
     extracted_dir = os.path.join(os.getcwd(), "extracted_components")
     os.makedirs(extracted_dir, exist_ok=True)
     for filename in component_filenames:
@@ -62,6 +79,18 @@ def install_vmware_fedora():
     bundle_installer = f"VMware-Workstation-{pkgver}-{buildver}.{CARCH}.bundle"
     run_command(f"chmod +x {bundle_installer}")
 
+    # # Step 4: Install VMware host modules
+    # install_vmware_modules()
+
+    # Step 5: Run the installer with extracted components
+    print("Running the VMware Workstation installer with extracted components...")
+    extracted_components = [os.path.join(extracted_dir, filename) for filename in os.listdir(extracted_dir)]
+    install_command = f"sudo ./{bundle_installer} --console --required --eulas-agreed " + " ".join(
+        [f'--install-component "{os.path.abspath(filename)}"' for filename in extracted_components]
+    )
+    run_command(install_command)
+
+    # Step 6: Install necessary dependencies
     print("Installing dependencies...")
     dependencies = [
         "kernel-devel",
@@ -73,19 +102,11 @@ def install_vmware_fedora():
     ]
     run_command(f"sudo dnf install -y {' '.join(dependencies)}")
     
-    # Step 4: Run the installer with component installation
-    print("Running the VMware Workstation installer with components...")
-    extracted_components = [os.path.join(extracted_dir, filename) for filename in os.listdir(extracted_dir)]
-    install_command = f"sudo ./{bundle_installer} --console --required --eulas-agreed " + " ".join(
-        [f'--install-component "{os.path.abspath(filename)}"' for filename in extracted_components]
-    )
-    run_command(install_command)
-
-
+    # Step 7: Compile kernel modules
     print("Compiling kernel modules...")
-    run_command("sudo vmware-modconfig --console --install-all")
+    install_vmware_modules()
     
-    # Step 7: Enable and start VMware services
+    # Step 8: Enable and start VMware services
     print("Enabling and starting VMware services...")
     services = [
         "vmware-networks.service",
@@ -95,7 +116,7 @@ def install_vmware_fedora():
     for service in services:
         run_command(f"sudo systemctl enable --now {service}")
     
-    # Step 8: Add the user to the vmware group
+    # Step 9: Add the user to the vmware group
     user = os.getlogin()
     print(f"Adding {user} to the vmware group...")
     run_command(f"sudo usermod -aG vmware {user}")
@@ -104,9 +125,3 @@ def install_vmware_fedora():
 
 if __name__ == "__main__":
     install_vmware_fedora()
-
-#     I need the use this:
-#     git clone -b tmp/workstation-17.5.2-k6.9.1 https://github.com/nan0desu/vmware-host-modules.git
-# cd vmware-host-modules/
-# sudo make tarballs && sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/
-# sudo vmware-modconfig --console --install-all
