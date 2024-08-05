9fbc0ec711b60b7d94571841e98e0338a6dc59f7
Hakan İSMAİL <hakanismail53@gmail.com>
2024-08-05 13:07:52 +0300

    Update __vmware_workstation__.py

--- functions/__vmware_workstation__.py
+++ functions/__vmware_workstation__.py
@@ -1,9 +1,10 @@
-import os
+from io import BytesIO
+from os import makedirs, path, chdir, listdir
 from pathlib import Path
-import subprocess
-import shutil
+from subprocess import run
 import logging
-
+from zipfile import ZipFile
+import requests
 
 class VMwareInstaller:
     PKGVER = "17.5.2"
@@ -13,7 +14,7 @@ class VMwareInstaller:
     BASE_URL = f"https://softwareupdate.vmware.com/cds/vmw-desktop/ws/{PKGVER}/{BUILDVER}/linux/packages"
     BUNDLE_URL = f"https://softwareupdate.vmware.com/cds/vmw-desktop/ws/{PKGVER}/{BUILDVER}/linux/core/VMware-Workstation-{PKGVER}-{BUILDVER}.{CARCH}.bundle.tar"
     BUNDLE_FILENAME = f"VMware-Workstation-{PKGVER}-{BUILDVER}.{CARCH}.bundle.tar"
-    CACHE_DIR = Path(os.path.expanduser("~")) / ".cache" / "vcandy" / "VMware"
+    CACHE_DIR = Path(path.expanduser("~")) / ".cache" / "vcandy" / "VMware"
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
     COMPONENT_FILENAMES = [
         f"vmware-tools-linux-{TOOLS_VERSION}.{CARCH}.component.tar",
@@ -24,7 +25,7 @@ class VMwareInstaller:
         f"vmware-tools-winPre2k-{TOOLS_VERSION}.{CARCH}.component.tar",
         f"vmware-tools-winPreVista-{TOOLS_VERSION}.{CARCH}.component.tar",
     ]
-    EXTRACTED_DIR = os.path.join(CACHE_DIR, "extracted_components")
+    EXTRACTED_DIR = path.join(CACHE_DIR, "extracted_components")
     DEPENDENCIES = []
     PACKAGE_MANAGER = ""
     PACKAGE_NAME = "vmware-host-modules"
@@ -36,9 +37,9 @@ class VMwareInstaller:
         "vmware-networks.path",
         "vmware-usbarbitrator.path",
     }
-    GITHUB_HOST_MODULES_REPO_URL = "https://github.com/mkubecek/vmware-host-modules.git"
+    GITHUB_HOST_MODULES_REPO_URL = "https://github.com/mkubecek/vmware-host-modules"
     GITHUB_HOST_MODULES_BRANCH = "tmp/workstation-17.5.0-k6.8"
-    GITHUB_REPO_URL = "https://github.com/Hakanbaban53/Container-and-Virtualization-Installer"
+    GITHUB_REPO_URL = "https://github.com/Hakanbaban53/Virtual-CANDY"
     GITHUB_BRANCH = "main"
 
     def __init__(self, hide, action, linux_distro):
@@ -49,7 +50,6 @@ class VMwareInstaller:
         self.hide = hide
         self.action = action
         self.linux_distro = linux_distro
-
         if self.linux_distro == "fedora":
             self.PACKAGE_MANAGER = "dnf"
             self.DEPENDENCIES = [
@@ -75,10 +75,19 @@ class VMwareInstaller:
                 "patch",
                 "net-tools",
             ]
-            kernel_version = subprocess.run(
-                "uname -r", shell=True, text=True
-            ).stdout.strip()
-            self.DEPENDENCIES.append(f"linux-headers-{kernel_version}")
+            try:
+                kernel_version_process = run(
+                    "uname -r", shell=True, text=True, capture_output=True
+                )
+                if kernel_version_process.returncode == 0:
+                    kernel_version = kernel_version_process.stdout.strip()
+                    self.DEPENDENCIES.append(f"linux-headers-{kernel_version}")
+                else:
+                    print(
+                        f"Error retrieving kernel version: {kernel_version_process.stderr}"
+                    )
+            except Exception as e:
+                print(f"Exception occurred while retrieving kernel version: {e}")
 
         if self.action == "install":
             self.install_vmware()
@@ -87,79 +96,98 @@ class VMwareInstaller:
 
     def setup_logging(self):
         """Setup logging configuration."""
-        logging.basicConfig(level=logging.INFO,
-                            format='%(levelname)s: %(message)s',
-                            handlers=[logging.FileHandler(f"{self.CACHE_DIR}/vmware_installer.log"),
-                                      logging.StreamHandler()])
-        
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(levelname)s: %(message)s",
+            handlers=[
+                logging.FileHandler(f"{self.CACHE_DIR}/vmware_installer.log"),
+                logging.StreamHandler(),
+            ],
+        )
 
     def run_command(self, command):
         """Run a shell command and logging. Error its output."""
-        result = subprocess.run(
-            command, shell=True, text=True, stderr=self.hide, stdout=self.hide
+        result = run(
+            command, shell=True, text=True, stderr=self.hide,
+            stdout=self.hide
         )
         if result.returncode != 0:
             logging.error(f"Command failed: {command}")
-            return False
-        return True
+            logging.error(f"Error output: {result.stderr}")
+            return False, result.stderr
+        return True, ""
 
     def download_file(self, url, filename):
         """Download a file from a URL using wget."""
         logging.info(f"Downloading {filename} from {url}...")
-        self.run_command(f"wget -O {os.path.join(self.CACHE_DIR, filename)} {url}")
+        self.run_command(f"wget -O {path.join(self.CACHE_DIR, filename)} {url}")
         logging.info(f"Downloaded {filename}.\n")
 
     def extract_tar(self, filename):
         """Extract a tar file."""
         logging.info(f"Extracting {filename} to {self.EXTRACTED_DIR}...")
         self.run_command(
-            f"tar -xf {os.path.join(self.CACHE_DIR, filename)} -C {self.EXTRACTED_DIR}"
+            f"tar -xf {path.join(self.CACHE_DIR, filename)} -C {self.EXTRACTED_DIR}"
         )
         self.run_command(f"find {self.EXTRACTED_DIR} -name '*.xml' -type f -delete")
 
-    def clone_repository(self, repo_url, branch, destination):
-        """Clone a Git repository."""
-        logging.info(f"Cloning repository from {repo_url} to {destination}...")
-        if not os.path.exists(destination):
-            os.makedirs(destination)
-        clone_commands = [
-            f"git clone -b {branch} {repo_url} {destination}"
-        ]
-        for command in clone_commands:
-            self.run_command(command)
+    def download_and_extract_zip(
+        self, repo_url, branch, folder_path=None, extract_to=None
+    ):
+        """Download and extract a GitHub repository or a specific folder."""
+        logging.info(f"Downloading repository from {repo_url}...")
+        zip_url = f"{repo_url}/archive/refs/heads/{branch}.zip"
+        response = requests.get(zip_url)
+        if response.status_code != 200:
+            logging.error(
+                f"Failed to download the repository. Status code: {response.status_code}"
+            )
+            return
 
-    def sparse_checkout(self, repo_url, branch, folder_to_clone, clone_location):
-        """Perform a sparse checkout from a Git repository."""
-        self.clone_repository(repo_url, branch, clone_location)
-        os.chdir(clone_location)
-
-        commands = [
-            "git sparse-checkout init --cone",
-            f"git sparse-checkout set {folder_to_clone}",
-            f"git checkout {branch}",  # Specify the branch explicitly here
-        ]
-
-        for command in commands:
-            self.run_command(command)
-
-        logging.info(
-            f"Folder {folder_to_clone} has been cloned to {clone_location}/{folder_to_clone}"
-        )
+        with ZipFile(BytesIO(response.content)) as zip_file:
+            if folder_path:
+                logging.info(f"Extracting the folder '{folder_path}'...")
+                for member in zip_file.namelist():
+                    if member.startswith(
+                        f"{repo_url.split('/')[-1]}-{branch}/{folder_path}"
+                    ):
+                        relative_path = path.relpath(
+                            member, f"{repo_url.split('/')[-1]}-{branch}"
+                        )
+                        target_path = path.join(extract_to, relative_path)
+                        if not member.endswith("/"):
+                            makedirs(path.dirname(target_path), exist_ok=True)
+                            with open(target_path, "wb") as f:
+                                f.write(zip_file.read(member))
+                        else:
+                            makedirs(target_path, exist_ok=True)
+                logging.info(f"Extracted folder '{folder_path}' to {extract_to}.")
+            else:
+                logging.info(f"Extracting the entire repository to {extract_to}...")
+                zip_file.extractall(extract_to)
+                logging.info(f"Extracted repository to {extract_to}.")
 
     def install_vmware_modules(self):
         """Install VMware modules."""
 
-        os.chdir(f"{self.CACHE_DIR}/vmware_host_modules")
-        logging.info("Making and copying vmmon.tar and vmnet.tar...")
-        self.run_command("make tarballs")
-        self.run_command(
-            f"sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/"
-        )
+        source_dir = f"{self.CACHE_DIR}/vmware-host-modules-tmp-workstation-17.5.0-k6.8"
+        dest_dir = "/usr/lib/vmware/modules/source/"
+        folders_to_copy = ["vmmon-only", "vmnet-only"]
 
-        logging.info("Extracting vmmon.tar and vmnet.tar...")
-        os.chdir("/usr/lib/vmware/modules/source/")
-        self.run_command("sudo tar -xvf vmmon.tar")
-        self.run_command("sudo tar -xvf vmnet.tar")
+        chdir(source_dir)
+        logging.info("Copying vmmon and vmnet folders...")
+
+        for folder in folders_to_copy:
+            src_folder = path.join(source_dir, folder)
+            dest_folder = path.join(dest_dir, folder)
+
+            if not path.exists(dest_folder):
+                self.run_command(f"sudo mkdir -p {dest_folder}")
+
+            logging.info(f"Copying {src_folder} to {dest_folder}...")
+            self.run_command(f"sudo cp -r {src_folder} {dest_folder}")
+
+        logging.info("Folders copied successfully.")
 
         logging.info("Creating directories for DKMS...")
         self.run_command(
@@ -174,14 +202,15 @@ class VMwareInstaller:
             f"sudo cp -r vmnet-only /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/"
         )
 
+
         logging.info("Copying Makefile and dkms.conf to DKMS directory...")
         self.run_command(
-            f"sudo cp -r {self.CACHE_DIR}/vmware_files/vmware_files/DKMS_files/Makefile /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/"
+            f"sudo cp -r {self.CACHE_DIR}/vmware_files/DKMS_files/Makefile /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/"
         )
 
         logging.info("Creating DKMS configuration for vmware-host-modules...")
         with open(
-            f"{self.CACHE_DIR}/vmware_files/vmware_files/DKMS_files/dkms.conf", "r"
+            f"{self.CACHE_DIR}/vmware_files/DKMS_files/dkms.conf", "r"
         ) as template_file:
             dkms_conf_template = template_file.read()
 
@@ -202,10 +231,10 @@ class VMwareInstaller:
 
         logging.info("Applying patches...")
         self.run_command(
-            f"sudo patch -p2 -d /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/vmmon-only < {self.CACHE_DIR}/vmware_files/vmware_files/DKMS_files/vmmon.patch"
+            f"sudo patch -N -p2 -d /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/vmmon-only < {self.CACHE_DIR}/vmware_files/DKMS_files/vmmon.patch"
         )
         self.run_command(
-            f"sudo patch -p2 -d /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/vmnet-only < {self.CACHE_DIR}/vmware_files/vmware_files/DKMS_files/vmnet.patch"
+            f"sudo patch -N -p2 -d /usr/src/{self.PACKAGE_NAME}-{self.PACKAGE_VERSION}/vmnet-only < {self.CACHE_DIR}/vmware_files/DKMS_files/vmnet.patch"
         )
 
         logging.info("Adding and building vmware-host-modules module with DKMS...")
@@ -222,17 +251,18 @@ class VMwareInstaller:
         logging.info("Running vmware-modconfig to install all modules...")
         self.run_command("sudo vmware-modconfig --console --install-all")
 
-        os.chdir("..")  # Return to previous directory
+        chdir("..")  # Return to previous directory
 
     def copy_service_files(self):
         """Copy systemd service files for VMware."""
         for filename in self.SERVICES:
-            self.run_command(f"sudo cp {self.CACHE_DIR}/vmware_files/vmware_files/services/{filename} /etc/systemd/system/{filename}")
+            self.run_command(
+                f"sudo cp {self.CACHE_DIR}/vmware_files/services/{filename} /etc/systemd/system/{filename}"
+            )
             logging.info(f"Copied {filename}.")
 
         # Reload systemd daemon to apply changes
         self.run_command("sudo systemctl daemon-reload")
-        
 
     def install_dependencies(self):
         """Install necessary dependencies for VMware."""
@@ -249,34 +279,34 @@ class VMwareInstaller:
 
         logging.info("\nStep 2: Clone the required repositories...")
         logging.info(f"Cloning {self.PACKAGE_NAME} repository...")
-        self.clone_repository(
-            self.GITHUB_HOST_MODULES_REPO_URL,
-            self.GITHUB_HOST_MODULES_BRANCH,
-            f"{self.CACHE_DIR}/vmware_host_modules"
+        self.download_and_extract_zip(
+            repo_url=self.GITHUB_HOST_MODULES_REPO_URL,
+            branch=self.GITHUB_HOST_MODULES_BRANCH,
+            extract_to=self.CACHE_DIR,
         )
 
         logging.info("Getting the DKMS modules")
-        self.sparse_checkout(
-            self.GITHUB_REPO_URL,
-            self.GITHUB_BRANCH,
-            "vmware_files",
-            f"{self.CACHE_DIR}/vmware_files",
+        self.download_and_extract_zip(
+            repo_url=self.GITHUB_REPO_URL,
+            branch=self.GITHUB_BRANCH,
+            folder_path="vmware_files",
+            extract_to=self.CACHE_DIR,
         )
 
         logging.info(
             "\nStep 3: Downloading the VMware Workstation installer and components..."
         )
-        os.makedirs(self.CACHE_DIR, exist_ok=True)
+        makedirs(self.CACHE_DIR, exist_ok=True)
         self.download_file(self.BUNDLE_URL, self.BUNDLE_FILENAME)
         for url, filename in zip(self.COMPONENT_URLS, self.COMPONENT_FILENAMES):
             self.download_file(url, filename)
 
         logging.info("\nStep 4: Extracting the bundle file...")
         self.run_command(
-            f"tar -xf {os.path.join(self.CACHE_DIR, self.BUNDLE_FILENAME)} -C {self.CACHE_DIR}"
+            f"tar -xf {path.join(self.CACHE_DIR, self.BUNDLE_FILENAME)} -C {self.CACHE_DIR}"
         )
 
-        os.makedirs(self.EXTRACTED_DIR, exist_ok=True)
+        makedirs(self.EXTRACTED_DIR, exist_ok=True)
         for filename in self.COMPONENT_FILENAMES:
             self.extract_tar(filename)
 
@@ -284,20 +314,20 @@ class VMwareInstaller:
         bundle_installer = (
             f"VMware-Workstation-{self.PKGVER}-{self.BUILDVER}.{self.CARCH}.bundle"
         )
-        self.run_command(f"chmod +x {os.path.join(self.CACHE_DIR, bundle_installer)}")
+        self.run_command(f"chmod +x {path.join(self.CACHE_DIR, bundle_installer)}")
 
         logging.info(
             "\nStep 6: Running the VMware Workstation installer with extracted components..."
         )
         extracted_components = [
-            os.path.join(self.EXTRACTED_DIR, filename)
-            for filename in os.listdir(self.EXTRACTED_DIR)
+            path.join(self.EXTRACTED_DIR, filename)
+            for filename in listdir(self.EXTRACTED_DIR)
         ]
         install_command = (
-            f"sudo {os.path.join(self.CACHE_DIR, bundle_installer)} --console --required --eulas-agreed "
+            f"sudo {path.join(self.CACHE_DIR, bundle_installer)} --console --required --eulas-agreed "
             + " ".join(
                 [
-                    f'--install-component "{os.path.abspath(filename)}"'
+                    f'--install-component "{path.abspath(filename)}"'
                     for filename in extracted_components
                 ]
             )
@@ -313,7 +343,9 @@ class VMwareInstaller:
         logging.info("\nStep 9: Set up the network adapters...")
         self.run_command("sudo chmod a+rw /dev/vmnet*")
 
-        logging.info(f"VMware installation and setup on {self.linux_distro} is complete.")
+        logging.info(
+            f"VMware installation and setup on {self.linux_distro} is complete."
+        )
 
     def uninstall_vmware(self):
         """Uninstall VMware Workstation."""
@@ -329,7 +361,7 @@ class VMwareInstaller:
 
         for service in self.SERVICES:
             service_file = f"/etc/systemd/system/{service}"
-            if os.path.exists(service_file):
+            if path.exists(service_file):
                 self.run_command(f"sudo rm {service_file}")
 
         logging.info("\nStep 3: Removing VMware modules from DKMS...")
@@ -339,11 +371,15 @@ class VMwareInstaller:
 
         logging.info("\nStep 4: Running the uninstallation script...")
         uninstall_script = "/usr/bin/vmware-installer"
-        if os.path.exists(uninstall_script):
+        if path.exists(uninstall_script):
             self.run_command(
                 f'echo "yes" | sudo {uninstall_script} --uninstall-product vmware-workstation'
             )
 
         logging.info("\nStep 5: Removing extracted components directory...")
-        if os.path.exists(self.EXTRACTED_DIR):
-            shutil.rmtree(self.EXTRACTED_DIR)
+        if path.exists(self.EXTRACTED_DIR):
+            self.run_command(f"sudo rm -rf {self.EXTRACTED_DIR}")
+
+
+if __name__ == "__main__":
+    VMwareInstaller(hide=None, action="install", linux_distro="fedora")
